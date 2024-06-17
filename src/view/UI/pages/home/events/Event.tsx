e337f59116d4b4d2a9a37706fc16f960f99ecfbe
afdavletkulov <afdavletkulov@rttv.ru>
2024-06-17 17:11:27 -0300

    fix

--- src/view/UI/pages/home/events/Event.tsx
+++ src/view/UI/pages/home/events/Event.tsx
@@ -1,39 +1,18 @@
-import Slider from "react-slick";
 import css from "./styles.module.scss";
+import test from "../../../assets/testImg/sp3.jpeg";
+import Button from "@mui/material/Button";
 
 function CenterMode() {
-  const settings = {
-    className: "center",
-    centerMode: true,
-    infinite: true,
-    centerPadding: "60px",
-    slidesToShow: 1,
-    speed: 500,
-    dots: false,
-    arrows: false,
-  };
   return (
-    <div className="slider-container">
-      <Slider {...settings}>
-        <div className={css.wrap}>
-          <div className={css.content}>1</div>
-        </div>
-        <div className={css.wrap}>
-          <div className={css.content}>2</div>
-        </div>
-        <div className={css.wrap}>
-          <div className={css.content}>3</div>
-        </div>
-        <div className={css.wrap}>
-          <div className={css.content}>4</div>
-        </div>
-        <div className={css.wrap}>
-          <div className={css.content}>5</div>
-        </div>
-        <div className={css.wrap}>
-          <div className={css.content}>6</div>
-        </div>
-      </Slider>
+    <div className={css.container}>
+      <h1>Событие недели</h1>
+      <span>башкортостан</span>
+      <div className={css.eventBlock}>
+        <img src={test} className={css.eventImg} />
+      </div>
+      <Button className={css.button} color="primary" variant="contained">
+        Отмена
+      </Button>
     </div>
   );
 }
