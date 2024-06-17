fe36b868f98a5f1a04e416ba50ce8b940b4b0f1c
afdavletkulov <afdavletkulov@rttv.ru>
2024-06-17 17:39:40 -0300

    fix

--- src/view/UI/pages/home/events/Event.tsx
+++ src/view/UI/pages/home/events/Event.tsx
@@ -5,10 +5,15 @@ import Button from "@mui/material/Button";
 function CenterMode() {
   return (
     <div className={css.container}>
-      <h1>Событие недели</h1>
-      <span>башкортостан</span>
+      <div className={css.title}>Событие недели</div>
+      <span className={css.subtitle}>башкортостан</span>
       <div className={css.eventBlock}>
         <img src={test} className={css.eventImg} />
+        <div className={css.eventInfo}>
+          <div className={css.price}>RUB 3500</div>
+          <div className={css.place}>Гафурийский район, Уклы Кая</div>
+          <div className={css.date}>Сб, 22.06 - Вс, 23.06</div>
+        </div>
       </div>
       <Button className={css.button} color="primary" variant="contained">
         Отмена
