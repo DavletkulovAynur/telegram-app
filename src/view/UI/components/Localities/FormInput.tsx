f7814967931acd67b19eb7f2dae79b7edb5f98f2
afdavletkulov <afdavletkulov@rttv.ru>
2024-06-18 18:38:11 -0300

    fix

--- src/view/UI/components/Localities/FormInput.tsx
+++ src/view/UI/components/Localities/FormInput.tsx
@@ -1,8 +1,7 @@
 import { FC } from "react";
 import { TextField } from "@mui/material";
 import css from "./styles.module.scss";
-import LocationSearchingOutlinedIcon from "@mui/icons-material/LocationSearchingOutlined";
-// import MyLocationOutlinedIcon from "@mui/icons-material/MyLocationOutlined";
+
 import { Control, Controller } from "react-hook-form";
 import { IFormData, TYPE_POINT } from "../../containers/SearchBlock/types";
 
@@ -13,39 +12,30 @@ interface IProps {
 }
 
 const FormInput: FC<IProps> = ({ searchLocality, control, test }) => {
-  // const destinationRef = useRef<HTMLInputElement>(null);
-  // const originRef = useRef<HTMLInputElement>(null);
-
   //TODO: name + label
   return (
-    <div className={css.formContainer}>
-      <div className={css.formWrap}>
-        <div className={css.inputWrap}>
-          <LocationSearchingOutlinedIcon
-            className={css.icon}
-            fontSize="medium"
-          />
-          <Controller
-            name={test}
-            control={control}
-            render={({ field: { value, onChange, ...field } }) => (
-              <TextField
-                {...field}
-                // inputRef={originRef}
-                fullWidth
-                placeholder={test}
-                label={test}
-                variant="standard"
-                value={value.name}
-                onChange={(e) => {
-                  const newValue = { id: value.id, name: e.target.value };
-                  onChange(newValue);
-                  searchLocality(e.target.value);
-                }}
-              />
-            )}
-          />
-        </div>
+    <div className={css.formWrap}>
+      <div className={css.inputWrap}>
+        <Controller
+          name={test}
+          control={control}
+          render={({ field: { value, onChange, ...field } }) => (
+            <TextField
+              {...field}
+              // inputRef={originRef}
+              fullWidth
+              placeholder={test}
+              label={test}
+              variant="outlined"
+              value={value.name}
+              onChange={(e) => {
+                const newValue = { id: value.id, name: e.target.value };
+                onChange(newValue);
+                searchLocality(e.target.value);
+              }}
+            />
+          )}
+        />
       </div>
     </div>
   );
