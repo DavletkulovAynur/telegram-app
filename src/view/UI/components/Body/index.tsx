6487ea5d86a5cb4ac1c4bb80b663ab99b7c839e8
afdavletkulov <afdavletkulov@rttv.ru>
2024-06-18 15:53:38 -0300

    fix

--- src/view/UI/components/Body/index.tsx
+++ src/view/UI/components/Body/index.tsx
@@ -1,16 +1,16 @@
-import { FC, PropsWithChildren, useEffect } from "react";
+import { FC, PropsWithChildren } from "react";
 import css from "./styles.module.scss";
-import { useTelegram } from "../../hooks";
+// import { useTelegram } from "../../hooks";
 const Body: FC<PropsWithChildren> = ({ children }) => {
   //FIXME: вынести глобально
-  const tg = useTelegram();
-  useEffect(() => {
-    if (!tg) return;
-    tg.ready();
+  // const tg = useTelegram();
+  // useEffect(() => {
+  //   if (!tg) return;
+  //   tg.ready();
 
-    tg.setHeaderColor(tg.themeParams.secondary_bg_color as string);
-    tg.setBackgroundColor(tg.themeParams.secondary_bg_color as string);
-  }, []);
+  //   tg.setHeaderColor(tg.themeParams.secondary_bg_color as string);
+  //   tg.setBackgroundColor(tg.themeParams.secondary_bg_color as string);
+  // }, []);
   return (
     <div className={css.bodyWrapper}>
       <div>{children}</div>
