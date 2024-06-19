a67031dd361473e177a01387ce89cdb0cf8a2d02
afdavletkulov <afdavletkulov@rttv.ru>
2024-06-19 12:13:03 -0300

    fix

--- src/view/UI/AppRoot.tsx
+++ src/view/UI/AppRoot.tsx
@@ -1,27 +1,27 @@
 import { FC, StrictMode } from "react";
 import { observer } from "mobx-react-lite";
-import { useChangeTheme } from "./hooks";
+import { useTelegram } from "./hooks";
 import {
   CssBaseline,
   StyledEngineProvider,
   ThemeProvider,
 } from "@mui/material";
 import { ErrorBoundary } from "./pages/errorBoundaryPage";
-// import { Header } from "./components/Header";
 import Body from "./components/Body";
 import App from "./App.tsx";
 import { IAppInitConfig } from "./types.ts";
 
-//FIXME: тут определяется темя как исправить???
 export const AppRoot: FC<{ themes: IAppInitConfig["themes"] }> = observer(
   ({ themes }) => {
+    const tg = useTelegram();
+
     const [lightTheme, darkTheme] = themes;
-    // const { navigate } = useRouter();
-    const { theme } = useChangeTheme();
 
     return (
       <StrictMode>
-        <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+        <ThemeProvider
+          theme={tg?.colorScheme === "dark" ? darkTheme : lightTheme}
+        >
           <CssBaseline />
           <StyledEngineProvider injectFirst>
             <ErrorBoundary>
