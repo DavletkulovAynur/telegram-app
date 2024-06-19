802747f9726d168d1e3aa951a6b8cd95f0b76506
afdavletkulov <afdavletkulov@rttv.ru>
2024-06-19 13:56:29 -0300

    fix

--- src/view/UI/pages/home/taxiOffer/TaxiOffer.tsx
+++ src/view/UI/pages/home/taxiOffer/TaxiOffer.tsx
@@ -1,11 +1,23 @@
 import css from "./styles.module.scss";
 import loup from "../../../assets/testImg/loup.jpeg";
+import Button from "@mui/material/Button";
 
 const TaxiOffer = () => {
   return (
     <div className={css.container}>
-      <div>
-        
+      <div className={css.content}>
+        <div className={css.title}>Предложить агентство</div>
+        <div className={css.description}>
+          Мы будем благодарны за вашу помощь!
+        </div>
+        <Button
+          // onClick={onExpand}
+          // disabled={loading}
+          color="secondary"
+          variant="outlined"
+        >
+          Написать
+        </Button>
       </div>
       <img className={css.loup} src={loup}></img>
     </div>
