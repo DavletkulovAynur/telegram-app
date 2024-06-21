b82758e82de68f780010f47e5244f6e25df67fd7
afdavletkulov <afdavletkulov@rttv.ru>
2024-06-21 18:22:52 -0300

    fix

--- src/view/UI/pages/home/taxiOffer/TaxiOffer.tsx
+++ src/view/UI/pages/home/taxiOffer/TaxiOffer.tsx
@@ -1,26 +1,33 @@
 import css from "./styles.module.scss";
 import loup from "../../../assets/testImg/loup.jpeg";
 import Button from "@mui/material/Button";
+import FullScreenDialog from "./Dialog";
+// import { useState } from "react";
 
 const TaxiOffer = () => {
+  // const [open, setOpen] = useState(false);
+  const openPopup = () => {};
   return (
-    <div className={css.container}>
-      <div className={css.content}>
-        <div className={css.title}>Предложить агентство</div>
-        <div className={css.description}>
-          Мы будем благодарны за вашу помощь!
+    <>
+      <div className={css.container} onClick={openPopup}>
+        <div className={css.content}>
+          <div className={css.title}>Предложить агентство</div>
+          <div className={css.description}>
+            Мы будем благодарны за вашу помощь!
+          </div>
+          <Button
+            // onClick={onExpand}
+            // disabled={loading}
+            color="secondary"
+            variant="outlined"
+          >
+            Написать
+          </Button>
         </div>
-        <Button
-          // onClick={onExpand}
-          // disabled={loading}
-          color="secondary"
-          variant="outlined"
-        >
-          Написать
-        </Button>
+        <img className={css.loup} src={loup}></img>
       </div>
-      <img className={css.loup} src={loup}></img>
-    </div>
+      <FullScreenDialog />
+    </>
   );
 };
 
